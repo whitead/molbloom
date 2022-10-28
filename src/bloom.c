09352b89e64312f0b1f8204971f3b3259b21c960
Andrew White <white.d.andrew@gmail.com>
2022-10-28 15:12:13 -0400

    Added read/write code for bloom filter

--- src/bloom.c
+++ src/bloom.c
@@ -1,149 +1,84 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <math.h>
+#include "bloom.h"
 
-#define VERSION "0.1"
-#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
-
-uint64_t FNV1a(const char *str)
-{
-  uint64_t hash = 14695981039346656037ULL;
-  for (int i = 0; str[i]; i++)
-  {
-    hash ^= str[i];
-    hash *= 1099511628211ULL;
-  }
-  return hash;
-}
-
-// bloom filter data
-typedef struct
-{
-  uint32_t k; // number of hash functions
-  uint32_t m; // size of bit array
-  char *data;
-} bloom_t;
-
-// create a new bloom filter
-bloom_t *bloom_new(uint32_t size, uint32_t n)
-{
-
-  // check we can index with uint32_t
-  if (size > 0xffffffff)
-  {
-    fprintf(stderr, "bloom_new: size too large\n");
-    return NULL;
-  }
-
-  bloom_t *b = malloc(sizeof(bloom_t));
-  b->m = size;
-  // 8 bits per byte
-  b->data = calloc(size / 8, 1);
-
-  // compute number of hash functions
-  b->k = min(32u, (char)(b->m / n * log(2)));
-
-  // print info and false positive rate
-  printf("bloom_new: size=%u bits, MB=%2f, n=%u k=%u fp=%f\n", b->m, ((float)b->m) / 1024 / 1024 / 8, n, b->k, pow(1 - exp(-(float)(b->k * n) / (b->m)), b->k));
-
-  return b;
-}
-
-// free a bloom filter
-void bloom_free(bloom_t *b)
-{
-  free(b->data);
-  free(b);
-}
-
-// add a string to a bloom filter
-void bloom_add(bloom_t *b, char *s)
-{
-  uint64_t h = FNV1a(s);
-  // split into two 32 bit hashes
-  uint32_t h1 = h & 0xffffffff;
-  uint32_t h2 = h >> 32;
-  for (int i = 0; i < b->k; i++)
-  {
-    // Building a Better Bloom Filter
-    h = (h1 + i * h2) % b->m;
-    b->data[h / 8] |= 1 << (h % 8);
-  }
-}
-
-// check if a string is in a bloom filter
-int bloom_check(bloom_t *b, char *s)
-{
-  uint64_t h = FNV1a(s);
-  // split into two 32 bit hashes
-  uint32_t h1 = h & 0xffffffff;
-  uint32_t h2 = h >> 32;
-  for (int i = 0; i < b->k; i++)
-  {
-    h = (h1 + i * h2) % b->m;
-    if (!(b->data[h / 8] & (1 << (h % 8))))
-      return 0;
-  }
-  return 1;
-}
-
-// write file
-void bloom_write(bloom_t *b, char *filename)
-{
-  FILE *f = fopen(filename, "wb");
-  fwrite(b->data, 1, b->m / 8, f);
-  fclose(f);
-}
-
-int main(int argc, char *argv[])
-{
-  char *ifile;
-  // process arguments to get input file
-  ifile = NULL;
-
-  if (argc != 3)
-  {
-    printf("Usage: %s <input file> <# of MB>\n", argv[0]);
+int main(int argc, char *argv[]) {
+  char *ifile, *ofile;
+  uint32_t size, n = 0;
+  if (!(argc == 4 || argc == 5)) {
+    printf(
+        "Usage: %s <input file> <# of MB> <name> <optional: approx number of "
+        "elements>\n",
+        argv[0]);
     return 1;
   }
 
-  printf("Info: You are running version %s of fastz\n", VERSION);
-  ifile = argv[1];
-
-  // start by counting number of elements
-  FILE *fp;
-  char *line = NULL;
+  FILE *ifp = NULL, *ofp = NULL;
+  char *line = NULL, *token = NULL, *name = NULL;
   size_t len = 0;
   size_t read;
-  size_t n = 0;
-  fp = fopen(ifile, "r");
-  if (fp == NULL)
-    exit(EXIT_FAILURE);
-  while ((read = getline(&line, &len, fp)) != -1)
-    n += 1;
+  bloom_t *b;
+  unsigned int i = 0;
 
-  // get size from MB string to number of bits
-  uint32_t size = (uint32_t)(atof(argv[2]) * 1024 * 1024 * 8);
+  printf("Info: You are running version %s of fastz\n", VERSION);
+  ifile = argv[1];
+  size = (uint32_t)(atof(argv[2]) * 1024 * 1024 * 8);
+  name = argv[3];
+  ofile = strdup(name);
+  strcat(ofile, ".bloom");
 
-  bloom_t *b = bloom_new(size, n);
-
-  // rewind file and add to bloom filter
-  rewind(fp);
-  while ((read = getline(&line, &len, fp)) != -1)
-  {
-    bloom_add(b, line);
+  ifp = fopen(ifile, "r");
+  if (ifp == NULL) exit(EXIT_FAILURE);
+  if (argc == 5)
+    n = (uint32_t)atoi(argv[4]);
+  else {
+    while ((read = getline(&line, &len, ifp)) != -1) n += 1;
+    rewind(ifp);
   }
 
+  // check if there is an existing bloom filter
+  ofp = fopen(ofile, "r");
+  if (ofp != NULL) {
+    printf("%s already exists -- loading\n", ofile);
+    bloom_read(&b, ofile);
+  } else {
+    b = bloom_new(size, n, name);
+  }
+
+
+  // add to bloom filter
+  printf("Building bloom filter\n");
+  while ((read = getline(&line, &len, ifp)) != -1) {
+    i += 1;
+    if (i % 10000 == 0) printf("\r%u", i);
+    // read to first space
+    token = strtok(line, " ");
+    bloom_add(b, token);
+  }
+  printf("\r%u Added\n", i);
+
   // check that one element is in
-  printf("bloom_check in: %s\n", bloom_check(b, "Cc1ccc(N2CC[C@@H](NS(=O)(=O)c3ccccc3C)C2=O)cc1C") ? "true" : "false");
+  printf("bloom_check in: %s\n",
+         bloom_check(b, token)
+             ? "true"
+             : "false");
 
   // check that one element is not in
-  printf("bloom_check not in: %s\n", bloom_check(b, "CC(C)(C)cb1ccc2occ(CC(=O)Nc3ccccc3F)c2c1") ? "true" : "false");
+  printf("bloom_check not in: %s\n",
+         bloom_check(b, "CC(C)(C)cXX1ccc2occ(CC(=O)Nc3ccccc3F)c2c1") ? "true"
+                                                                    : "false");
 
-  // write bloom filter to file
-  bloom_write(b, "bloom.bin");
+  if(ofp != NULL)
+    fclose(ofp);
+  // reopen in write mode
+  ofp = fopen(ofile, "w");
+  // overwrite bloom filter to file
+  printf("Writing bloom filter to %s\n", ofile);
+  bloom_write(b, ofile);
+
+
+  free(line);
+  fclose(ifp);
+  fclose(ofp);
+  bloom_free(b);
 
   return (0);
 }
---
+++ src/bloom.h
@@ -0,0 +1,132 @@
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#define VERSION "0.1.0"
+#define FILE_VERSION 1u
+#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define max(X, Y) (((X) > (Y)) ? (X) : (Y))
+
+uint64_t FNV1a(const char *str) {
+  uint64_t hash = 14695981039346656037ULL;
+  for (int i = 0; str[i]; i++) {
+    hash ^= str[i];
+    hash *= 1099511628211ULL;
+  }
+  return hash;
+}
+
+// bloom filter data
+typedef struct {
+  uint32_t k;  // number of hash functions
+  uint32_t m;  // size of bit array
+  char name[32];  // name of filter
+  char *data;
+} bloom_t;
+
+// create a new bloom filter
+bloom_t *bloom_new(uint32_t size, uint32_t n, const char *name) {
+  // check we can index with uint32_t
+  if (size > 0xffffffff) {
+    fprintf(stderr, "bloom_new: size too large\n");
+    return NULL;
+  }
+
+  bloom_t *b = malloc(sizeof(bloom_t));
+  b->m = size;
+  // 8 bits per byte
+  b->data = calloc(size / 8, 1);
+  if(strlen(name) > 32) {
+    fprintf(stderr, "bloom_new: name too long\n");
+    return NULL;
+  } else {
+    strcpy(b->name, name);
+  }
+
+  // compute number of hash functions
+  b->k = max(8u, min(32u, (char)(b->m / n * log(2))));
+
+  // print info and false positive rate
+  printf("bloom_new: %s size=%u bits, MB=%2f, n=%u k=%u fp=%f\n", b->name, b->m,
+         ((float)b->m) / 1024 / 1024 / 8, n, b->k,
+         pow(1 - exp(-(float)(b->k * n) / (b->m)), b->k));
+
+  return b;
+}
+
+// free a bloom filter
+void bloom_free(bloom_t *b) {
+  free(b->data);
+  free(b);
+}
+
+// add a string to a bloom filter
+void bloom_add(bloom_t *b, char *s) {
+  uint64_t h = FNV1a(s);
+  // split into two 32 bit hashes
+  uint32_t h1 = h & 0xffffffff;
+  uint32_t h2 = h >> 32;
+  for (int i = 0; i < b->k; i++) {
+    // Building a Better Bloom Filter
+    h = (h1 + i * h2) % b->m;
+    b->data[h / 8] |= 1 << (h % 8);
+  }
+}
+
+// check if a string is in a bloom filter
+int bloom_check(bloom_t *b, char *s) {
+  uint64_t h = FNV1a(s);
+  // split into two 32 bit hashes
+  uint32_t h1 = h & 0xffffffff;
+  uint32_t h2 = h >> 32;
+  for (int i = 0; i < b->k; i++) {
+    h = (h1 + i * h2) % b->m;
+    if (!(b->data[h / 8] & (1 << (h % 8)))) return 0;
+  }
+  return 1;
+}
+
+// write file
+void bloom_write(bloom_t *b, char *filename) {
+  FILE *f = fopen(filename, "wb");
+  // write magic number
+  fwrite("BLOOM", 1, 5, f);
+  // write version
+  putc(FILE_VERSION, f);
+  // write k and m
+  fwrite(&b->k, sizeof(u_int32_t), 1, f);
+  fwrite(&b->m, sizeof(u_int32_t), 1, f);
+  fwrite(b->data, 1, b->m / 8, f);
+  fclose(f);
+}
+
+void bloom_read(bloom_t **b, char *filename) {
+    FILE *f = fopen(filename, "rb");
+    char magic[5];
+    fread(magic, 1, 5, f);
+    if(strcmp(magic, "BLOOM") != 0) {
+        fprintf(stderr, "bloom_read: invalid magic number\n");
+        return;
+    }
+    uint8_t version = getc(f);
+    if(version != FILE_VERSION) {
+        fprintf(stderr, "bloom_read: invalid version number\n");
+        return;
+    }
+    uint32_t k, m;
+    fread(&k, sizeof(uint32_t), 1, f);
+    fread(&m, sizeof(uint32_t), 1, f);
+    *b = malloc(sizeof(bloom_t));
+    (*b)->k = k;
+    (*b)->m = m;
+    (*b)->data = malloc(m / 8);
+    fread((*b)->data, 1, m / 8, f);
+    fclose(f);
+
+    printf("bloom_read: %s size=%u bits, MB=%2f, k=%u\n", (*b)->name, (*b)->m,
+         ((float)(*b)->m) / 1024 / 1024 / 8, (*b)->k);
+}
