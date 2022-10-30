f999f28469aa26baa508332fa782b689c35aebc0
Matthew O'Meara <maom@umich.edu>
2022-10-30 16:48:39 -0400

    add initial buy_similar function

--- molbloom/__init__.py
+++ molbloom/__init__.py
@@ -64,3 +64,118 @@ def buy(smiles, instock=True, canonicalize=False):
             raise ImportError("To canonicalize SMILES, rdkit is required.")
         smiles = Chem.MolToSmiles(Chem.MolFromSmiles(smiles), canonical=True)
     return smiles in filters[name]
+
+def buy_similar(
+    smiles,
+    db = "REAL-Database-22Q1.smi.anon",
+    small_world_args = {
+        "dist" : 4,
+        "sdist" : 12,
+        "tdn" : 6,
+        "tup" : 6,
+        "rdn" : 6,
+        "rup" : 2,
+        "ldn" : 2,
+        "lup" : 2,
+        "maj" : 6,
+        "min" : 6,
+        "sub" : 6,
+        "scores" : "Atom%20Alignment,ECFP4,Daylight"},
+    n_retries = 5,
+    verbose = False):
+
+    import urllib
+    import urllib.parse
+    import urllib.request
+    import time
+    import pandas
+
+    try:
+        sw_server_path = "https://sw.docking.org/search/"
+        if len(small_world_args) > 0:
+            args = []
+            for arg, value in small_world_args.items():
+                args.append(f"{arg}={value}")
+            args = "&" + "&".join(args)
+        else:
+            args = ""
+        query_url = f"{sw_server_path}submit?smi={smiles}&db={db}{args}"
+    except:
+        raise f"Failed to construct sw.docking.org query url for smiles '{smiles}'"
+
+    if verbose:
+        print(f"Querying ZINC Small World with url: {query_url}")
+
+    lines = None
+    http_status = None
+    for attempt_i in range(n_retries):
+        with urllib.request.urlopen(query_url) as response:
+            http_status = response.status
+            if http_status == 200:
+                lines = [line.decode('utf-8')[:-1] for line in response.readlines()]
+                break
+        if verbose:
+            print(f"HTTP request attempt {attempt_i} failed with status: {http_status} ...")
+        time.sleep(1)
+
+    if lines is None:
+        raise Exception(f"Failed to query sw.docking.org, HTTPS status: {http_status}")
+
+    sw_status = None
+    hlid = None
+    for line in lines:
+        if line == "": continue
+        line = line.replace("data:{", "").replace("}\n", "")
+        line = line.split(",")
+
+        for key_value in line:
+            if '"status":' in key_value:
+                sw_status = key_value.replace('"status":', "").replace('"', "")
+
+            if "hlid" in key_value:
+                hlid = key_value.replace("\"hlid\":", "")
+
+    if sw_status is None or sw_status != "END":
+        raise Exception(f"Small World query did not complete, status: {sw_status}")
+
+    try:
+        hlid = int(hlid)
+    except:
+        raise Exception(f"Expected small world query id to be an inteter, instead it was {hlid}")
+
+    results_args = "&".join([
+        f"hlid={hlid}",
+        "order[0][column]=0",
+        "columns[0][name]=alignment",
+        "order[0][dir]=asc",
+        "columns[1][name]=dist",
+        "columns[1][search][value]=0-12",
+        "columns[2][name]=ecfp4",
+        "columns[3][name]=daylight",
+        "columns[5][name]=mces"]
+        ).replace("[", "%5B").replace("]", "%5D")
+    results_url = f"{sw_server_path}export?{results_args}"
+
+    if verbose:
+        print(f"Getting results from ZINC Small World with url: {results_url}")
+
+    http_status = None
+    table = None
+    for attempt_i in range(n_retries):
+        with urllib.request.urlopen(results_url) as response:
+            if response is None:
+                time.sleep(1)
+                continue
+            http_status = response.status
+            if http_status == 200:
+                lines = [line.decode('utf-8')[:-1].split('\t') for line in response.readlines()]
+                table = pandas.DataFrame(columns = lines[0], data = lines[1:])
+
+        if verbose:
+            print(f"HTTP request attempt {attempt_i} failed with status: {http_status} ...")
+        time.sleep(1)
+
+    if http_status != 200:
+        raise Exception(f"Failed to get results with HTTP status: {http_status}")
+
+    return table
